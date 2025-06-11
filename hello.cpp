6b90763afc8bdcaa612ba62c6205d990eb955990
MADHURIMA BAG_MCA_2024-26_26_UEMK <madhurimabag4@gmail.com>
2025-06-11 12:40:07 +0530

    11/06/25

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+
+    // Ask for user input
+    cout << "Enter your name: ";
+    cin >> name;
+
+    // Print Hello message
+    cout << "Hello, " << name << "!" << endl;
+
+    return 0;
+}
+
