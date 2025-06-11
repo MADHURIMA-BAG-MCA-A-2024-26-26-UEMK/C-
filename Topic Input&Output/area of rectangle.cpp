a7ac09ec8816f60cb2e92b545026d73c483662b2
MADHURIMA BAG_MCA_2024-26_26_UEMK <madhurimabag4@gmail.com>
2025-06-11 13:38:43 +0530

    11/06/25

---
+++ Topic Input&Output/area of rectangle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float length, width;
+    cout << "Enter length and width: ";
+    cin >> length >> width;
+    float area = length * width;
+    cout << "Area of rectangle = " << area << endl;
+    return 0;
+}
