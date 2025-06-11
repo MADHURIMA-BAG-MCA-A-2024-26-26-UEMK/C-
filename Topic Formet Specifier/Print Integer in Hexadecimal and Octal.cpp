a3c6505a5bab06f93ee11a748ac1cc4a4f1a9cd7
MADHURIMA BAG_MCA_2024-26_26_UEMK <madhurimabag4@gmail.com>
2025-06-11 14:00:36 +0530

    11/06/25

---
+++ Topic Formet Specifier/Print Integer in Hexadecimal and Octal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int num = 255;
+
+    cout << "Decimal: " << num << endl;
+    cout << "Hexadecimal: " << hex << num << endl;
+    cout << "Octal: " << oct << num << endl;
+
+    return 0;
+}
