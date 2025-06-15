1b8316c13746f8a5729f8b1a30b1f9b6400700c8
MADHURIMA BAG_MCA_2024-26_26_UEMK <madhurimabag4@gmail.com>
2025-06-15 20:38:37 +0530

    15/06/25

---
+++ Challange 1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    
+    // Prompt the user for input (optional)
+    cout << "Enter three integers separated by spaces: ";
+    
+    // Read user input
+    cin >> a >> b >> c;
+    
+    // Calculate and print their sum
+    cout << "Sum: " << a + b + c << endl;
+    
+    return 0;
+}
+
